ee7a4dda6dace82f52b7d2fa496ae35ceac200ce
BenTStark <B.Ruf@hotmail.de>
2018-10-09 17:13:47 +0200

    Create container.view.jsx

---
+++ react_container/container.view.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class <Container>View extends Component {
+
+    render() {
+        return (
+            <div>
+              blank Container
+            </div>
+        )
+    }
+}
+export default <Container>View;
