83a1f47162646fad9cb1415f76d686aae8e239de
BenTStark <B.Ruf@hotmail.de>
2018-10-17 17:20:23 +0200

    Update component

---
+++ react_feature/featurePart.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function <FeaturePart>Component({
+  defaultProp
+}) {
+
+  return (
+    <div>
+      Empty Component
+    </div>
+  )
+}
+
+export default <FeaturePart>Component;
